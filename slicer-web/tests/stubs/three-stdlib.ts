2418f31ba89055f40ed8e53e9d79ac670a56d80c
Pedro Luiz Bortot Monteiro do Rosário <60971278+PedroBMR@users.noreply.github.com>
2025-10-03 10:05:00 -0300

    test: add geometry worker volume integration tests

---
+++ slicer-web/tests/stubs/three-stdlib.ts
@@ -0,0 +1,3 @@
+export { BufferGeometryUtils } from 'three/examples/jsm/utils/BufferGeometryUtils.js';
+export { STLLoader } from 'three/examples/jsm/loaders/STLLoader.js';
+export { ThreeMFLoader } from 'three/examples/jsm/loaders/3MFLoader.js';
