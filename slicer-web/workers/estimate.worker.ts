7e44d93f5e065261ebdb460f8a7d44049c4746dc
Pedro Luiz Bortot Monteiro do Rosário <60971278+PedroBMR@users.noreply.github.com>
2025-10-06 15:39:49 -0300

    feat: integrate estimator UI into main page

--- slicer-web/workers/estimate.worker.ts
+++ slicer-web/workers/estimate.worker.ts
@@ -1,6 +1,10 @@
 import { expose } from 'comlink';
 
-import { estimateAll as estimateAllFromLib, type EstimateBreakdown, type PrintParams } from '../lib/estimate';
+import {
+  estimateAll as estimateAllFromLib,
+  type EstimateBreakdown,
+  type PrintParams,
+} from '../lib/estimate';
 
 export interface EstimateWorkerApi {
   estimateAll(volumeModel_mm3: number, params?: Partial<PrintParams>): EstimateBreakdown;
